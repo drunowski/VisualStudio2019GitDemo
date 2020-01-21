650d2d7db32cd447d4562c54368fc9875c7b3d61
David Runowski <drunowski@usf.edu>
2020-01-21 11:26:47 -0500

    Commit the changes 1.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
